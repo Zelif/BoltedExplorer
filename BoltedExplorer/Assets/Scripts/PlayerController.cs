d25e3efd9b780cd50c979f81d767b7fe263b8153
Zelif <jarodk.1988@gmail.com>
2018-08-11 12:20:58 +1000

    Tut Add

---
+++ BoltedExplorer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [HideInInspector]
+    public bool facingRight = true;
+    [HideInInspector]
+    public bool jump = false;
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+   // public AudioClip[] jumpClips;
+    public float jumpForce = 300f;
+   // public AudioClip[] taunts;
+    public float tauntProbability = 50f;
+    public float tauntDelay = 1f;
+
+    private int tauntIndex;
+    private Transform groundCheck;
+    private bool grounded = false;
+   // private Animator anim;
+    private new Rigidbody2D rigidbody;
+
+    void Awake()
+    {
+        groundCheck = transform.Find("groundCheck");
+  //      anim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if( Input.GetButtonDown("Jump") && grounded )
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+     //   anim.SetFloat("Speed", Mathf.Abs(h));
+
+        if( h * rigidbody.velocity.x < maxSpeed )
+        {
+            rigidbody.AddForce(Vector2.right * h * moveForce);
+        }
+
+        if( Mathf.Abs(rigidbody.velocity.x) > maxSpeed )
+        {
+            rigidbody.velocity = new Vector2(Mathf.Sign(rigidbody.velocity.x) * maxSpeed, rigidbody.velocity.y);
+        }
+
+        if( h > 0 && !facingRight )
+        {
+            Flip();
+        }
+        else if( h < 0 && facingRight )
+        {
+            Flip();
+        }
+
+        if( jump )
+        {
+      //      anim.SetTrigger("Jump");
+
+      //      int i = Random.Range(0, jumpClips.Length);
+      //      AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
+
+            rigidbody.AddForce(new Vector2(0f, jumpForce));
+
+            jump = false;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ BoltedExplorer/Assets/Scripts/SceneControllers/SplashController.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour
+{
+    // TODO(Shaun): Refactor to only pass in textures instead of images for the image list
+
+    /* ----------------------------------------------------------------------------------------------------------------------------------------- */
+
+    #region Public Members
+
+    public float runTime = 5f;              // Display time for images
+    public float fadeTime = 1f;             // Fade in and out time
+    public bool fade = true;                // Enables or disables fade
+    public List<Image> imageList;           // List of images passed in
+    public Image fadeImage;                 // Fade cover passed in to hide image list
+
+    #endregion
+
+    /* ----------------------------------------------------------------------------------------------------------------------------------------- */
+
+    #region Private Members
+
+    private float displayTimer;             // Current timer for display image
+    private float fadeTimer;                // Current timer for fade cover
+    private bool isFading;                  // Overall fade state running
+    private bool fadeIn = true;             // State for fading in
+    private bool fadeOut = false;           // State for fading out
+    private int imageCount;                 // Count of the image list
+    private int imageIndex = 0;             // Current image index
+
+    #endregion
+
+    /* ----------------------------------------------------------------------------------------------------------------------------------------- */
+
+    #region Awake Function
+
+    #endregion
+
+    /* ----------------------------------------------------------------------------------------------------------------------------------------- */
+
+    #region Start Function
+
+    void Start()
+    {
+        displayTimer = runTime;
+
+        if( fade )
+        {
+            fadeTimer = fadeTime;
+            isFading = true;
+        }
+        else
+        {
+            DisableFadeImage();
+        }
+
+        imageCount = imageList.Count;
+    }
+
+    #endregion
+
+    /* ----------------------------------------------------------------------------------------------------------------------------------------- */
+
+    #region Update Function
+
+    void Update()
+    {
+        if( Input.anyKey )
+        {
+            SceneManager.LoadScene("Scenes/MainMenu");
+        }
+
+        if( isFading )
+        {
+            Fading();
+        }
+        else
+        {
+            DisplayImage();
+        }
+    }
+
+    #endregion
+
+    /* ----------------------------------------------------------------------------------------------------------------------------------------- */
+
+    #region Fixed Update Function
+
+    #endregion
+
+    /* ----------------------------------------------------------------------------------------------------------------------------------------- */
+
+    #region Functions
+
+    /// <summary>
+    /// Checks and calculates the time for the fade in and/or 
+    /// fade out.
+    /// </summary>
+    void Fading()
+    {
+        if( fadeIn )
+        {
+            fadeTimer -= Time.deltaTime;
+
+            if( fadeTimer <= 0 )
+            {
+                fadeIn = false;
+                isFading = false;
+                fadeOut = true;
+                fadeTimer = 0f;
+            }
+        }
+
+        if( fadeOut && isFading )
+        {
+            fadeTimer += Time.deltaTime;
+            if( fadeTimer >= fadeTime )
+            {
+                fadeOut = false;
+                CheckImages();
+                fadeIn = true;
+            }
+        }
+
+        Color tmp = fadeImage.color;
+        tmp.a = fadeTimer / fadeTime;
+        fadeImage.color = tmp;
+    }
+
+    /// <summary>
+    /// Checks and calculates if the time that the image is 
+    /// displayed is up.
+    /// </summary>
+    void DisplayImage()
+    {
+        displayTimer -= Time.deltaTime;
+
+        if( displayTimer <= 0.0f )
+        {
+            TimerEnded();
+        }
+    }
+
+    /// <summary>
+    /// Gets called after the display timer has finished. It
+    /// will either fade or check for the next image
+    /// </summary>
+    void TimerEnded()
+    {
+        displayTimer = runTime;
+
+        if( fade )
+        {
+            isFading = true;
+            fadeTimer = 0;
+        }
+        else
+        {
+            CheckImages();
+        }
+    }
+
+    /// <summary>
+    /// Checks if there are any more images to display. If not
+    /// it will load the main menu
+    /// </summary>
+    void CheckImages()
+    {
+        if( imageIndex + 1 < imageCount )
+        {
+            imageList[imageIndex].enabled = false;
+            imageIndex++;
+        }
+        else
+        {
+            SceneManager.LoadScene("Scenes/MainMenu");
+        }
+    }
+
+    /// <summary>
+    /// Disables the fade layer if fade is false
+    /// </summary>
+    void DisableFadeImage()
+    {
+        fadeImage.enabled = false;
+    }
+
+    #endregion
+
+    /* ----------------------------------------------------------------------------------------------------------------------------------------- */
+}
