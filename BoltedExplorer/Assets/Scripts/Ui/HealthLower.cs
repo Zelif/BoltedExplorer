a731a6b7e9038ecec234bd7c66ce1e044290056f
Zelif <jarodk.1988@gmail.com>
2018-08-12 02:31:46 +1000

    Health I guess

---
+++ BoltedExplorer/Assets/Scripts/Ui/HealthLower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthLower : MonoBehaviour {
+    public float health;
+    public float height;
+
+    private float originHeight;
+    private float originalWidth;
+    private RectTransform rectTransform;
+    // Use this for initialization
+    void Start () {
+        health = 100;
+        rectTransform = GetComponent<RectTransform>();
+        originalWidth = rectTransform.sizeDelta.x;
+        originHeight = rectTransform.sizeDelta.y;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        height = health /100 * originHeight;
+        rectTransform.sizeDelta = new Vector2(originalWidth, height);
+
+    }
+}
