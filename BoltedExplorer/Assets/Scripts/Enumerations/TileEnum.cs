69efbacaa8f91f849e6b8295ca81a090b921b35c
Shaun Crilley <shauncrilley@hotmail.com>
2018-08-11 14:12:02 +1000

    level 1 added with tiles

---
+++ BoltedExplorer/Assets/Scripts/Enumerations/TileEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+public enum TileType
+{
+    [Description("Unassigned")]
+    Unassigned = 0,
+    
+    [Description("Spawn Point")]
+    Spawn = 1,
+
+    [Description("End Goal")]
+    Goal = 2,
+
+    [Description("Solid Collider")]
+    Solid = 3,
+
+    [Description("Water")]
+    Water = 4,
+
+    [Description("Spikes")]
+    Spikes = 5
+}
